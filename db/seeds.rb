df7f60d70b7790d7556b0f5a15945dbdbb7ae1ac
Adyson-Lima <adyson_lima@yahoo.com.br>
2023-12-14 16:42:28 -0300

    seeds.rb adicionado

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,10 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+puts 'Gerando motorcycles...'
+
+5.times do |i|
+  Motorcycle.create(
+    name: ["bis", "harley", "shadow"].sample ,
+    category: "moto categorizada"
+    )
+end
+
+puts 'motorcycles gerados com sucesso!'
